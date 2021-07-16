fefdcdc4f8569d6240740d855f41968a72e738a8
Kris Raney <kristopher.raney@cloudkitchens.com>
2021-07-16 10:13:42 -0700

    Extract thrift, add 0.12 formula

---
+++ Formula/thrift@0.12.0_1.rb
@@ -0,0 +1,73 @@
+class ThriftAT01201 < Formula
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  url "https://www.apache.org/dyn/closer.cgi?path=/thrift/0.12.0/thrift-0.12.0.tar.gz"
+  sha256 "c336099532b765a6815173f62df0ed897528a9d551837d627c1f87fadad90428"
+  license "Apache-2.0"
+
+  head do
+    url "https://github.com/apache/thrift.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+  end
+
+  bottle do
+    rebuild 1
+    sha256 cellar: :any, big_sur: "aaa413ef1e58db4335d74adf78e4a3b25d518e7e41d0c6540c077f927d0fc478"
+  end
+
+  depends_on "bison" => :build
+  depends_on "boost" => [:build, :test]
+  depends_on "openssl@1.1"
+
+  def install
+    system "./bootstrap.sh" unless build.stable?
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+      --without-erlang
+      --without-haskell
+      --without-java
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-python
+      --without-ruby
+      --without-swift
+    ]
+
+    ENV.cxx11 if ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.thrift").write <<~'EOS'
+      service MultiplicationService {
+        i32 multiply(1:i32 x, 2:i32 y),
+      }
+    EOS
+
+    system "#{bin}/thrift", "-r", "--gen", "cpp", "test.thrift"
+
+    system ENV.cxx, "-std=c++11", "gen-cpp/MultiplicationService.cpp",
+      "gen-cpp/MultiplicationService_server.skeleton.cpp",
+      "-I#{include}/include",
+      "-L#{lib}", "-lthrift"
+  end
+end
